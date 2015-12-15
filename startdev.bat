c237368a2d8bd997c267cb59072503fbabfd5aaf
Jim Toth <jim@spooky.email>
2015-12-15 18:54:39 -0500

    Added startdev.bat.  Updated .gitignore: removed startdev.bat, added config.bat

---
+++ startdev.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+REM spooky.email windows dev environment startup script
+REM 	opens two command prompts to the main directory, the sublime text
+REM 	project, starts the node server, and opens local www dev root in a
+REM		browser window
+REM		calls config.bat, which sets variables used during startup
+
+CALL config.bat
+
+START cmd.exe /K "cd %dev_root%"
+START cmd.exe /K "cd %dev_root% && npm start"
+START "" /d %sublime_text_root% sublime_text.exe
+START explorer.exe "%dev_root%"
+START "" /d %browser_root% "%local_www_root%"
